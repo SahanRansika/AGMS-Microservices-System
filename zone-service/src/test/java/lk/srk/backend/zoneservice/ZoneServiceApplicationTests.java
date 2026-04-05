47ebfa741663c6d904b6cef22b80adbb1b786cf4
SahanRansika <sahanransika07@gmail.com>
2026-04-05 12:08:33 +0530

    AGMS Microservices config server is completed🎉

---
+++ zone-service/src/test/java/lk/srk/backend/zoneservice/ZoneServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.srk.backend.zoneservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
